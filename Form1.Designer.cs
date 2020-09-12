0fbc300f8e4543ced8f8d9ab6f33e73f78eecf89
kodythach <kodythach@gmail.com>
2020-09-12 16:57:41 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace CraftopiaWorldTransferApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BrowseButton1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BrowseButton2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.transferWorldBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BrowseButton1
+            // 
+            this.BrowseButton1.Location = new System.Drawing.Point(272, 40);
+            this.BrowseButton1.Name = "BrowseButton1";
+            this.BrowseButton1.Size = new System.Drawing.Size(100, 23);
+            this.BrowseButton1.TabIndex = 0;
+            this.BrowseButton1.Text = "Browse";
+            this.BrowseButton1.UseVisualStyleBackColor = true;
+            this.BrowseButton1.Click += new System.EventHandler(this.BrowseButton1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Enter path of Source SaveData";
+            this.textBox1.Size = new System.Drawing.Size(254, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PlaceholderText = "Enter path of Destination SaveData";
+            this.textBox2.Size = new System.Drawing.Size(254, 23);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // BrowseButton2
+            // 
+            this.BrowseButton2.Location = new System.Drawing.Point(272, 92);
+            this.BrowseButton2.Name = "BrowseButton2";
+            this.BrowseButton2.Size = new System.Drawing.Size(100, 23);
+            this.BrowseButton2.TabIndex = 0;
+            this.BrowseButton2.Text = "Browse";
+            this.BrowseButton2.UseVisualStyleBackColor = true;
+            this.BrowseButton2.Click += new System.EventHandler(this.BrowseButton2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source Save to Transfer from";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Destination Save to Transfer to";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Source";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(285, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Destination";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(13, 149);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(110, 274);
+            this.listBox1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(262, 149);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(110, 274);
+            this.listBox2.TabIndex = 5;
+            // 
+            // transferWorldBtn
+            // 
+            this.transferWorldBtn.Location = new System.Drawing.Point(131, 248);
+            this.transferWorldBtn.Name = "transferWorldBtn";
+            this.transferWorldBtn.Size = new System.Drawing.Size(123, 23);
+            this.transferWorldBtn.TabIndex = 6;
+            this.transferWorldBtn.Text = "Transfer World →";
+            this.transferWorldBtn.UseVisualStyleBackColor = true;
+            this.transferWorldBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 425);
+            this.Controls.Add(this.transferWorldBtn);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BrowseButton2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BrowseButton1);
+            this.Name = "Form1";
+            this.Text = "World Transfer App";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BrowseButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button BrowseButton2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button transferWorldBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace CraftopiaWorldTransferApp
+{
+    public partial class Form1 : Form
+    {
+        JObject srcSavedData;
+        JObject dstSavedData;
+
+        public JObject DecompressSavedData(FileInfo fileToDecompress)
+        {
+            using (FileStream originalFileStream = fileToDecompress.OpenRead())
+            {
+                using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                {
+                    StreamReader reader = new StreamReader(decompressionStream);
+                    JObject savedData = JObject.Parse(reader.ReadToEnd());
+                    return savedData;
+                }
+            }
+        }
+
+        public void CompressNewSavedData(JObject dstSavedData)
+        {
+            string currentFileName = textBox2.Text;
+            using (FileStream compressedFileStream = File.Create(currentFileName))
+            {
+                using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                {
+                    byte[] byteArray = Encoding.UTF8.GetBytes(dstSavedData.ToString());
+                    MemoryStream dstStream = new MemoryStream(byteArray);
+                    dstStream.CopyTo(compressionStream);
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BrowseButton1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse .ocs files",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DefaultExt = "ocs",
+                Filter = "ocs files (*.ocs)|*.ocs",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                textBox1.Text = openFileDialog1.FileName;
+                srcSavedData = DecompressSavedData(new FileInfo(openFileDialog1.FileName));
+                foreach (var world in srcSavedData["WorldListSave"]["value"])
+                {
+                    listBox1.Items.Add(world["name"]);
+                }
+            }
+        }
+
+        private void BrowseButton2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse .ocs files",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DefaultExt = "ocs",
+                Filter = "ocs files (*.ocs)|*.ocs",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listBox2.Items.Clear();
+                textBox2.Text = openFileDialog1.FileName;
+                dstSavedData = DecompressSavedData(new FileInfo(openFileDialog1.FileName));
+                foreach (var world in dstSavedData["WorldListSave"]["value"])
+                {
+                    listBox2.Items.Add(world["name"]);
+                }
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                JToken sourceWorld = srcSavedData["WorldListSave"]["value"][listBox1.SelectedIndex];
+                dstSavedData["WorldListSave"]["value"][0].AddBeforeSelf(sourceWorld);
+                CompressNewSavedData(dstSavedData);
+                listBox2.Items.Insert(0, listBox1.SelectedItem.ToString());
+                MessageBox.Show("World has been successfully transferred to the " +
+                    $"designated save file:\n({textBox2.Text}).", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Click on a world to transfer from the source box "+
+                    "and click 'Transfer' to import it over.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
