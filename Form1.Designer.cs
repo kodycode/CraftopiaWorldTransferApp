2ea68db3117f958e05c2c1b01d244bcdbdd50ddf
kodythach <kodythach@gmail.com>
2020-09-12 21:08:18 -0400

    Version 1.1
    - Added a way to export worlds as a JSON file that you can import into a
      saved data file
    - More checks added to produce error prompts
    - Cleaned up code name conventions

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,16 +29,17 @@
         private void InitializeComponent()
         {
             this.BrowseButton1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.srcSaveDataTxt = new System.Windows.Forms.TextBox();
+            this.dstSaveDataTxt = new System.Windows.Forms.TextBox();
             this.BrowseButton2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.srcSaveTransferLbl = new System.Windows.Forms.Label();
+            this.dstSaveTransferLbl = new System.Windows.Forms.Label();
+            this.srcLabel = new System.Windows.Forms.Label();
+            this.dstLabel = new System.Windows.Forms.Label();
+            this.srcList = new System.Windows.Forms.ListBox();
+            this.dstList = new System.Windows.Forms.ListBox();
             this.transferWorldBtn = new System.Windows.Forms.Button();
+            this.ExportBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BrowseButton1
@@ -51,23 +52,23 @@
             this.BrowseButton1.UseVisualStyleBackColor = true;
             this.BrowseButton1.Click += new System.EventHandler(this.BrowseButton1_Click);
             // 
-            // textBox1
+            // srcSaveDataTxt
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 40);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PlaceholderText = "Enter path of Source SaveData";
-            this.textBox1.Size = new System.Drawing.Size(254, 23);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.srcSaveDataTxt.Location = new System.Drawing.Point(12, 40);
+            this.srcSaveDataTxt.Name = "srcSaveDataTxt";
+            this.srcSaveDataTxt.PlaceholderText = "Enter path of Source SaveData";
+            this.srcSaveDataTxt.Size = new System.Drawing.Size(254, 23);
+            this.srcSaveDataTxt.TabIndex = 1;
+            this.srcSaveDataTxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // dstSaveDataTxt
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 92);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PlaceholderText = "Enter path of Destination SaveData";
-            this.textBox2.Size = new System.Drawing.Size(254, 23);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.dstSaveDataTxt.Location = new System.Drawing.Point(12, 92);
+            this.dstSaveDataTxt.Name = "dstSaveDataTxt";
+            this.dstSaveDataTxt.PlaceholderText = "Enter path of Destination SaveData";
+            this.dstSaveDataTxt.Size = new System.Drawing.Size(254, 23);
+            this.dstSaveDataTxt.TabIndex = 1;
+            this.dstSaveDataTxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // BrowseButton2
             // 
@@ -79,59 +80,59 @@
             this.BrowseButton2.UseVisualStyleBackColor = true;
             this.BrowseButton2.Click += new System.EventHandler(this.BrowseButton2_Click);
             // 
-            // label1
+            // srcSaveTransferLbl
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 15);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Source Save to Transfer from";
+            this.srcSaveTransferLbl.AutoSize = true;
+            this.srcSaveTransferLbl.Location = new System.Drawing.Point(12, 22);
+            this.srcSaveTransferLbl.Name = "srcSaveTransferLbl";
+            this.srcSaveTransferLbl.Size = new System.Drawing.Size(157, 15);
+            this.srcSaveTransferLbl.TabIndex = 2;
+            this.srcSaveTransferLbl.Text = "Source Save to Transfer from";
             // 
-            // label2
+            // dstSaveTransferLbl
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 74);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(166, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Destination Save to Transfer to";
+            this.dstSaveTransferLbl.AutoSize = true;
+            this.dstSaveTransferLbl.Location = new System.Drawing.Point(12, 74);
+            this.dstSaveTransferLbl.Name = "dstSaveTransferLbl";
+            this.dstSaveTransferLbl.Size = new System.Drawing.Size(166, 15);
+            this.dstSaveTransferLbl.TabIndex = 2;
+            this.dstSaveTransferLbl.Text = "Destination Save to Transfer to";
             // 
-            // label3
+            // srcLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(43, 127);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 15);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Source";
+            this.srcLabel.AutoSize = true;
+            this.srcLabel.Location = new System.Drawing.Point(43, 127);
+            this.srcLabel.Name = "srcLabel";
+            this.srcLabel.Size = new System.Drawing.Size(43, 15);
+            this.srcLabel.TabIndex = 4;
+            this.srcLabel.Text = "Source";
             // 
-            // label4
+            // dstLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(285, 127);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 15);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Destination";
+            this.dstLabel.AutoSize = true;
+            this.dstLabel.Location = new System.Drawing.Point(285, 127);
+            this.dstLabel.Name = "dstLabel";
+            this.dstLabel.Size = new System.Drawing.Size(67, 15);
+            this.dstLabel.TabIndex = 4;
+            this.dstLabel.Text = "Destination";
             // 
-            // listBox1
+            // srcList
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(13, 149);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(110, 274);
-            this.listBox1.TabIndex = 5;
+            this.srcList.FormattingEnabled = true;
+            this.srcList.ItemHeight = 15;
+            this.srcList.Location = new System.Drawing.Point(13, 149);
+            this.srcList.Name = "srcList";
+            this.srcList.Size = new System.Drawing.Size(110, 274);
+            this.srcList.TabIndex = 5;
             // 
-            // listBox2
+            // dstList
             // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 15;
-            this.listBox2.Location = new System.Drawing.Point(262, 149);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(110, 274);
-            this.listBox2.TabIndex = 5;
+            this.dstList.FormattingEnabled = true;
+            this.dstList.ItemHeight = 15;
+            this.dstList.Location = new System.Drawing.Point(262, 149);
+            this.dstList.Name = "dstList";
+            this.dstList.Size = new System.Drawing.Size(110, 274);
+            this.dstList.TabIndex = 5;
             // 
             // transferWorldBtn
             // 
@@ -141,23 +142,34 @@
             this.transferWorldBtn.TabIndex = 6;
             this.transferWorldBtn.Text = "Transfer World →";
             this.transferWorldBtn.UseVisualStyleBackColor = true;
-            this.transferWorldBtn.Click += new System.EventHandler(this.button1_Click);
+            this.transferWorldBtn.Click += new System.EventHandler(this.transferWorldBtn_Click);
+            // 
+            // ExportBtn
+            // 
+            this.ExportBtn.Location = new System.Drawing.Point(131, 278);
+            this.ExportBtn.Name = "ExportBtn";
+            this.ExportBtn.Size = new System.Drawing.Size(123, 23);
+            this.ExportBtn.TabIndex = 7;
+            this.ExportBtn.Text = "Export World";
+            this.ExportBtn.UseVisualStyleBackColor = true;
+            this.ExportBtn.Click += new System.EventHandler(this.ExportBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(385, 425);
+            this.Controls.Add(this.ExportBtn);
             this.Controls.Add(this.transferWorldBtn);
-            this.Controls.Add(this.listBox2);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dstList);
+            this.Controls.Add(this.srcList);
+            this.Controls.Add(this.dstLabel);
+            this.Controls.Add(this.srcLabel);
+            this.Controls.Add(this.dstSaveTransferLbl);
+            this.Controls.Add(this.srcSaveTransferLbl);
             this.Controls.Add(this.BrowseButton2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dstSaveDataTxt);
+            this.Controls.Add(this.srcSaveDataTxt);
             this.Controls.Add(this.BrowseButton1);
             this.Name = "Form1";
             this.Text = "World Transfer App";
@@ -170,16 +182,17 @@
         #endregion
 
         private System.Windows.Forms.Button BrowseButton1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox srcSaveDataTxt;
+        private System.Windows.Forms.TextBox dstSaveDataTxt;
+        private System.Windows.Forms.Label srcSaveTransferLbl;
+        private System.Windows.Forms.Label dstSaveTransferLbl;
+        private System.Windows.Forms.Label srcLabel;
+        private System.Windows.Forms.Label dstLabel;
         private System.Windows.Forms.Button BrowseButton2;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox srcList;
+        private System.Windows.Forms.ListBox dstList;
         private System.Windows.Forms.Button transferWorldBtn;
+        private System.Windows.Forms.Button ExportBtn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,8 +16,10 @@ namespace CraftopiaWorldTransferApp
 {
     public partial class Form1 : Form
     {
+        string srcSaveDataContent;
         JObject srcSavedData;
         JObject dstSavedData;
+        bool isJSON = false;
 
         public JObject DecompressSavedData(FileInfo fileToDecompress)
         {
@@ -34,7 +36,7 @@ namespace CraftopiaWorldTransferApp
 
         public void CompressNewSavedData(JObject dstSavedData)
         {
-            string currentFileName = textBox2.Text;
+            string currentFileName = dstSaveDataTxt.Text;
             using (FileStream compressedFileStream = File.Create(currentFileName))
             {
                 using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
@@ -66,11 +68,11 @@ namespace CraftopiaWorldTransferApp
             OpenFileDialog openFileDialog1 = new OpenFileDialog
             {
                 InitialDirectory = @"C:\",
-                Title = "Browse .ocs files",
+                Title = "Browse .ocs/.json files",
                 CheckFileExists = true,
                 CheckPathExists = true,
                 DefaultExt = "ocs",
-                Filter = "ocs files (*.ocs)|*.ocs",
+                Filter = "json files (*.json)|*.json|ocs files (*.ocs)|*.ocs",
                 FilterIndex = 2,
                 RestoreDirectory = true,
                 ReadOnlyChecked = true,
@@ -78,12 +80,25 @@ namespace CraftopiaWorldTransferApp
             };
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                listBox1.Items.Clear();
-                textBox1.Text = openFileDialog1.FileName;
-                srcSavedData = DecompressSavedData(new FileInfo(openFileDialog1.FileName));
-                foreach (var world in srcSavedData["WorldListSave"]["value"])
+                srcList.Items.Clear();
+                srcSaveDataTxt.Text = openFileDialog1.FileName;
+                if (openFileDialog1.FileName.EndsWith(".json"))
                 {
-                    listBox1.Items.Add(world["name"]);
+                    srcSaveDataContent = File.ReadAllText(srcSaveDataTxt.Text);
+                    isJSON = true;
+                    ExportBtn.Enabled = false;
+                    srcList.Items.Add(new FileInfo(openFileDialog1.FileName).Name.Replace(".json", ""));
+                    srcList.SetSelected(0, true);
+                }
+                else
+                {
+                    isJSON = false;
+                    ExportBtn.Enabled = true;
+                    srcSavedData = DecompressSavedData(new FileInfo(openFileDialog1.FileName));
+                    foreach (var world in srcSavedData["WorldListSave"]["value"])
+                    {
+                        srcList.Items.Add(world["name"]);
+                    }
                 }
             }
         }
@@ -105,12 +120,12 @@ namespace CraftopiaWorldTransferApp
             };
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                listBox2.Items.Clear();
-                textBox2.Text = openFileDialog1.FileName;
+                dstList.Items.Clear();
+                dstSaveDataTxt.Text = openFileDialog1.FileName;
                 dstSavedData = DecompressSavedData(new FileInfo(openFileDialog1.FileName));
                 foreach (var world in dstSavedData["WorldListSave"]["value"])
                 {
-                    listBox2.Items.Add(world["name"]);
+                    dstList.Items.Add(world["name"]);
                 }
             }
         }
@@ -120,21 +135,64 @@ namespace CraftopiaWorldTransferApp
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void transferWorldBtn_Click(object sender, EventArgs e)
         {
-            if (listBox1.SelectedIndex != -1)
+            if (File.Exists(dstSaveDataTxt.Text))
             {
-                JToken sourceWorld = srcSavedData["WorldListSave"]["value"][listBox1.SelectedIndex];
-                dstSavedData["WorldListSave"]["value"][0].AddBeforeSelf(sourceWorld);
-                CompressNewSavedData(dstSavedData);
-                listBox2.Items.Insert(0, listBox1.SelectedItem.ToString());
-                MessageBox.Show("World has been successfully transferred to the " +
-                    $"designated save file:\n({textBox2.Text}).", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (srcList.SelectedIndex != -1)
+                {
+                    JToken sourceWorld;
+                    if (isJSON)
+                    {
+                        sourceWorld = JToken.Parse(srcSaveDataContent);
+                    }
+                    else
+                    {
+                        sourceWorld = srcSavedData["WorldListSave"]["value"][srcList.SelectedIndex];
+                    }
+                    dstSavedData["WorldListSave"]["value"][0].AddBeforeSelf(sourceWorld);
+                    CompressNewSavedData(dstSavedData);
+                    dstList.Items.Insert(0, srcList.SelectedItem.ToString());
+                    MessageBox.Show("World has been successfully transferred to the " +
+                        $"designated save file:\n({dstSaveDataTxt.Text}).", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Click on a world to transfer from the source box " +
+                        "and click 'Transfer World' to import it over.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
             else
             {
-                MessageBox.Show("Click on a world to transfer from the source box "+
-                    "and click 'Transfer' to import it over.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Destination file path does not exist. Please enter a valid file path. ",
+                       "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ExportBtn_Click(object sender, EventArgs e)
+        {
+            if (!isJSON)
+            {
+                if (srcList.SelectedIndex != -1)
+                {
+                    JToken sourceWorld = srcSavedData["WorldListSave"]["value"][srcList.SelectedIndex];
+                    Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/world");
+                    File.WriteAllText($"{Directory.GetCurrentDirectory()}/world/{srcList.SelectedItem.ToString() + ".json"}", sourceWorld.ToString());
+                    MessageBox.Show("World has been successfully exported to:\n" +
+                        $"({Directory.GetCurrentDirectory()}+/world/{srcList.SelectedItem.ToString() + ".json"}).", "Success",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Click on a world to export from the source box " +
+                        "and click 'Export World' to export the world as a separate file.",
+                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("The World you want to export is already a JSON file",
+                       "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
